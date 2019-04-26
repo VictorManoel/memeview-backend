42f1d8e32425b7a7125b4359677ddbdaa774aa7d
Victor Manoel <victormsilva2001@gmail.com>
2019-04-26 13:37:08 -0300

    Controller function for once video

--- src/controllers/AppController.js
+++ src/controllers/AppController.js
@@ -4,14 +4,41 @@ class AppController {
 	// List videos of playlist
 	async list(req, res) {
 		try {
+			const { index = 1 } = req.query;
+			const pageToken = index == 1 ? "" : index;
+
 			const { data } = await youtube.playlistItems.list({
 				part: "snippet",
 				playlistId: process.env.PLAYLIST_ID,
 				maxResults: 12,
-				pageToken: ""
+				pageToken
 			});
 
 			return res.status(200).json(data);
+		} catch (error) {
+			console.log(error);
+			return res.status(400).json({ error: "Bad request" });
+		}
+	}
+
+	// Once video
+	async once(req, res) {
+		try {
+			const { id } = req.params;
+
+			const { data } = await youtube.videos.list({
+				part: "snippet",
+				maxResults: 1,
+				id
+			});
+
+			const item = data.items[0];
+
+			if (item) {
+				return res.status(200).json(item);
+			} else {
+				return res.status(404).json({ error: "Video not found" });
+			}
 		} catch (error) {
 			console.log(error);
 			return res.status(400).json({ error: "Bad request" });
--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,8 @@ const routes = require("express").Router();
 // Controller
 const AppController = require("./controllers/AppController");
 
+// Routes
 routes.get("/", AppController.list);
+routes.get("/:id", AppController.once);
 
 module.exports = routes;
